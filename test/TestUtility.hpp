8be183e1139746683c27c272eb9f6347849b1971
WojtekMs <wojtek.atmo@gmail.com>
2021-09-06 21:22:41 +0200

    TestUtility code review comments resolved
    Added Updatable concept to update_many
    Added std::ranges_iota_view to update_many

--- test/TestUtility.hpp
+++ test/TestUtility.hpp
@@ -1,14 +1,19 @@
 #pragma once
 #include <SFML/System/Vector2.hpp>
+#include <ranges>
 
 #include "Engine.hpp"
 #include "Tank.hpp"
 
 void expect_vec2f_eq(const sf::Vector2f& lhs, const sf::Vector2f& rhs);
 
-template <typename Updatable>
-void update_many(Updatable& updatable, int count) {
-  for (int i = 0; i < count; i++) {
+template <typename T>
+concept Updatable = requires(T a) {
+  a.update();
+};
+
+void update_many(Updatable auto& updatable, int count) {
+  for (int i : std::ranges::iota_view{0, count}) {
     updatable.update();
   }
 }
