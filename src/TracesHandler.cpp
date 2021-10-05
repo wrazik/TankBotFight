a08e714426d5daa53af9e16ece4b216b7f2b6bce
WojtekMs <wojtek.atmo@gmail.com>
2021-10-05 21:53:43 +0200

    TracesHandler can handle adding multiple moves with different angle

--- src/TracesHandler.cpp
+++ src/TracesHandler.cpp
@@ -1,15 +1,28 @@
 #include "TracesHandler.hpp"
+
 #include "utility.hpp"
 constexpr int tracesCount = 1;
 
-TracesHandler::TracesHandler(sf::Texture& texture, const sf::Vector2f& pos) : mTracks(texture), mStartingPosition(pos) {}
+void increaseSpriteHeight(sf::Sprite& sprite, int amount) {
+  const auto& rect = sprite.getTextureRect();
+  sprite.setTextureRect({rect.top, rect.left, rect.width, rect.height + amount});
+}
+
+TracesHandler::TracesHandler(sf::Texture& texture, const sf::Vector2f& pos)
+    : mTracks(texture), mStartingPosition(pos) {}
 
 void TracesHandler::update(const sf::Vector2f& move) {
-    sf::Sprite sprite(mTracks);
-    auto rect = sprite.getTextureRect();
-    sprite.setTextureRect({rect.top, rect.left, rect.width, hypot(move)});
-    sprite.setRotation(get_angle(move));
+  sf::Sprite sprite(mTracks);
+  const auto& rect = sprite.getTextureRect();
+  const float moveAngle = get_angle(move);
+  if (!mTraces.empty() && mTraces.back().getRotation() == moveAngle) {
+    auto& trace = mTraces.back();
+    increaseSpriteHeight(trace, hypot(move));
+  } else {
+    sprite.setTextureRect({rect.top, rect.left, rect.width, static_cast<int>(hypot(move))});
+    sprite.setRotation(moveAngle);
     mTraces.push_back(sprite);
+  }
 }
 
 const std::deque<sf::Sprite>& TracesHandler::getTraces() { return mTraces; }
--- test/TracesHandlerTests.cpp
+++ test/TracesHandlerTests.cpp
@@ -2,6 +2,7 @@
 
 #include "TestUtility.hpp"
 #include "TracesHandler.hpp"
+#include "utility.hpp"
 
 class TracesHandlerTest : public ::testing::Test {
  protected:
@@ -18,9 +19,59 @@ TEST_F(TracesHandlerTest, GivenFreshTracesHandler_ThenTracesShouldBeEmpty) {
   EXPECT_TRUE(actualTraces.empty());
 }
 
-TEST_F(TracesHandlerTest, GivenOneMove_ThenTracesShouldContainSprite) {
-  mHandler.update({3.f, 4.f});
-  auto actualTraces = mHandler.getTraces();
+TEST_F(TracesHandlerTest, GivenMove_ThenTracesShouldContainSpriteWithHeightEqualToMoveHypot) {
+  sf::Vector2f move{3.f, 4.f};
+  mHandler.update(move);
+  const float expectedRotation = get_angle(move);
+  const float expectedHeight = 5.f;
+  const auto& actualTraces = mHandler.getTraces();
 
-  EXPECT_EQ(1, actualTraces.size());
+  ASSERT_EQ(1, actualTraces.size());
+  const auto actualSpriteRotation = actualTraces.front().getRotation();
+  const auto actualSpriteHeight = actualTraces.front().getTextureRect().height;
+
+  EXPECT_EQ(expectedRotation, actualSpriteRotation);
+  EXPECT_EQ(expectedHeight, actualSpriteHeight);
+}
+
+TEST_F(TracesHandlerTest,
+       GivenMovesWithSameAngle_ThenTracesShouldContainSpriteWithHeightEqualToSumOfMoveHypot) {
+  sf::Vector2f move{3.f, 4.f};
+  mHandler.update(move);
+  mHandler.update(move);
+  const float expectedRotation = get_angle(move);
+  const float expectedHeight = 10.f;
+  const auto& actualTraces = mHandler.getTraces();
+
+  ASSERT_EQ(1, actualTraces.size());
+  const auto actualSpriteHeight = actualTraces.front().getTextureRect().height;
+  const float actualSpriteRotation = actualTraces.front().getRotation();
+
+  EXPECT_EQ(expectedRotation, actualSpriteRotation);
+  EXPECT_EQ(expectedHeight, actualSpriteHeight);
+}
+
+TEST_F(
+    TracesHandlerTest,
+    GivenMovesWithDifferentAngle_ThenTracesShouldContainSeperateSpritesWithHeightEqualToMoveHypot) {
+  sf::Vector2f move1{6.f, 8.f};
+  sf::Vector2f move2{3.f, -4.f};
+  mHandler.update(move1);
+  mHandler.update(move2);
+  const float expectedRotation1 = get_angle(move1);
+  const float expectedRotation2 = get_angle(move2);
+  const float expectedHeight1 = 10.f;
+  const float expectedHeight2 = 5.f;
+  const auto& actualTraces = mHandler.getTraces();
+
+  ASSERT_EQ(2, actualTraces.size());
+  const float actualSpriteRotation1 = actualTraces[0].getRotation();
+  const float actualSpriteRotation2 = actualTraces[1].getRotation();
+  const auto actualSpriteHeight1 = actualTraces[0].getTextureRect().height;
+  const auto actualSpriteHeight2 = actualTraces[1].getTextureRect().height;
+
+  EXPECT_EQ(expectedRotation1, actualSpriteRotation1);
+  EXPECT_EQ(expectedRotation2, actualSpriteRotation2);
+  EXPECT_EQ(expectedHeight1, actualSpriteHeight1);
+  EXPECT_EQ(expectedHeight2, actualSpriteHeight2);
 }
