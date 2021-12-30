d1fcbc5efbce4e8487fee5a02531fd086d14c703
WojtekMs <wojtek.atmo@gmail.com>
2021-12-30 19:11:53 +0100

    Clang tidy introduced into project CMake
    - ENABLE_CLANG_TIDY cmake option to create 'make tidy' target
    - CLANG_TIDY_VERSION option to define which version of clang tidy you wish to use
    
    Additionally function which creates `tidy` target can take list of files you wish to run the checks for. This is important option, because if any new files will be added then they have to specified.

---
+++ cmake/clang-tidy.cmake
@@ -0,0 +1,37 @@
+include_guard()
+
+function(add_tidy_target TIDY_VERSION)
+  set(options)
+  set(oneValueArgs)
+  set(multiValueArgs FILES)
+  cmake_parse_arguments(add_tidy_target "${options}" "${oneValueArgs}"
+                        "${multiValueArgs}" ${ARGN})
+
+  find_program(CLANG_TIDY_BIN clang-tidy-${TIDY_VERSION})
+  find_program(RUN_CLANG_TIDY_BIN run-clang-tidy-${TIDY_VERSION}.py)
+
+  if(CLANG_TIDY_BIN STREQUAL "CLANG_TIDY_BIN-NOTFOUND")
+    message(FATAL_ERROR "unable to locate clang-tidy-${TIDY_VERSION}")
+  endif()
+
+  if(RUN_CLANG_TIDY_BIN STREQUAL "RUN_CLANG_TIDY_BIN-NOTFOUND")
+    message(FATAL_ERROR "unable to locate run-clang-tidy-${TIDY_VERSION}.py")
+  endif()
+
+  list(
+    APPEND
+    RUN_CLANG_TIDY_BIN_ARGS
+    -clang-tidy-binary
+    ${CLANG_TIDY_BIN}
+    -header-filter=.*
+    -checks=clang-diagnostic-*,clang-analyzer-*,bugprone-*,cert-*,clang-analyzer-*,concurrency-*,cppcoreguidelines-*,hicpp-*,misc-*,modernize-*,performance-*,portability-*,readability-*
+    -p=${CMAKE_BINARY_DIR}
+    ${add_tidy_target_FILES}
+    )
+
+  add_custom_target(
+    tidy
+    COMMAND ${RUN_CLANG_TIDY_BIN} ${RUN_CLANG_TIDY_BIN_ARGS}
+    COMMENT "running clang tidy")
+
+endfunction(add_tidy_target)
