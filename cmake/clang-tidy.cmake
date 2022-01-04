a1beccb9841bfefd804a61197a9aad7778d20413
Wojciech <62173407+WojtekMs@users.noreply.github.com>
2022-01-04 09:35:24 +0100

    Clang-tidy added to Ubuntu CI (#34)
    * Tank does not move out of the board
    
    Tests & implementation
    
    - New border checking functions added to utility.hpp
    - Tank::update_position performs position checks based on functions from utility.hpp
    
    * Clang tidy introduced into project CMake
    
    - ENABLE_CLANG_TIDY cmake option to create 'make tidy' target
    - CLANG_TIDY_VERSION option to define which version of clang tidy you wish to use
    
    Additionally function which creates `tidy` target can take list of files you wish to run the checks for. This is important option, because if any new files will be added then they have to specified.
    
    * New CI pipeline for clang-tidy
    
    First attempt to run tidy target on macos-latest
    
    * Change clang-tidy platform to ubuntu
    
    * Checking available clang-tidy versions
    
    * Added clang-tidy installation step
    
    - clang tidy installation instructions taken from the official llvm autoinstall script: https://apt.llvm.org/llvm.sh
    
    * Fix sudo error in clang-tidy installation
    
    * Insert clang tidy to ubuntu_gcc.yaml pipeline
    
    * Trying to debug run-clang-tidy error
    
    * Explicit paths added to find_program in clang-tidy.cmake
    
    * Another try to resolve CI clang-tidy error
    
    * Clang tidy CI compilation error fixed
    
    clang-tidy.cmake refactored a little

---
+++ cmake/clang-tidy.cmake
@@ -0,0 +1,29 @@
+include_guard()
+
+function(add_tidy_target TIDY_VERSION)
+  set(options)
+  set(oneValueArgs)
+  set(multiValueArgs FILES)
+  cmake_parse_arguments(add_tidy_target "${options}" "${oneValueArgs}"
+                        "${multiValueArgs}" ${ARGN})
+
+  find_program(CLANG_TIDY_BIN clang-tidy-${TIDY_VERSION} REQUIRED)
+  find_program(RUN_CLANG_TIDY_BIN NAMES run-clang-tidy-${TIDY_VERSION}.py run-clang-tidy-${TIDY_VERSION} REQUIRED)
+
+  list(
+    APPEND
+    RUN_CLANG_TIDY_BIN_ARGS
+    -clang-tidy-binary
+    ${CLANG_TIDY_BIN}
+    -header-filter=.*
+    -checks=clang-diagnostic-*,clang-analyzer-*,bugprone-*,cert-*,clang-analyzer-*,concurrency-*,cppcoreguidelines-*,hicpp-*,misc-*,modernize-*,performance-*,portability-*,readability-*
+    -p=${CMAKE_BINARY_DIR}
+    ${add_tidy_target_FILES}
+    )
+
+  add_custom_target(
+    tidy
+    COMMAND ${RUN_CLANG_TIDY_BIN} ${RUN_CLANG_TIDY_BIN_ARGS}
+    COMMENT "running clang tidy")
+
+endfunction(add_tidy_target)
