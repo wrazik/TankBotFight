d083cf88df1af1e577a4fda54a5d1ba40a45441d
wrazik <wojciech.razik@gmail.com>
2023-08-11 15:00:21 +0200

    Fix ci

--- src/GameObject.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-
-struct GameObject {
-  virtual sf::Vector2f get_pos() const = 0;
-  virtual float get_radius() const = 0;
-  virtual ~GameObject() = default;
-};
