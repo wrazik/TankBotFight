501779e295e9c76ef3d093cfc40a4dbfbf1339b7
Dawid Biliński <dawid.bilinski.99@gmail.com>
2023-09-03 11:33:37 +0200

    add TankHealthBar class

---
+++ src/Tank/TankHealthBar.cpp
@@ -0,0 +1,41 @@
+#include "TankHealthBar.hpp"
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <string>
+#include "Files.hpp"
+
+constexpr float HEALTHBAR_Y_OFFSET = -50.f;
+
+TankHealthBar::TankHealthBar(unsigned int total_health)
+    : mTotalHealth(total_health), mHealth(total_health) {
+  mFont.loadFromFile(files::asset_path() + "DejaVuSans.ttf");
+  setup_text();
+  update_string();
+}
+
+void TankHealthBar::set_position(const sf::Vector2f& pos) {
+  mText.setPosition(pos.x, pos.y + HEALTHBAR_Y_OFFSET);
+}
+
+void TankHealthBar::set_health(unsigned int health) {
+  mHealth = health;
+  update_string();
+}
+
+void TankHealthBar::draw(sf::RenderWindow& window) {
+  window.draw(mText);
+}
+
+void TankHealthBar::setup_text() {
+  mText.setFont(mFont);
+  mText.setCharacterSize(10);
+  mText.setOrigin(20.f, 20.f);
+  mText.setFillColor(sf::Color::Black);
+}
+
+void TankHealthBar::update_string() {
+  mText.setString(std::to_string(mHealth) + " / " + std::to_string(mTotalHealth));
+  sf::FloatRect textRect = mText.getLocalBounds();
+  mText.setOrigin(textRect.left + textRect.width / 2.f,
+                  textRect.top + textRect.height / 2.f);
+}
+
---
+++ src/Tank/TankHealthBar.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Text.hpp>
+#include <SFML/System/Vector2.hpp>
+
+class TankHealthBar {
+ public:
+  TankHealthBar(unsigned int total_health);
+
+  void set_position(const sf::Vector2f& pos);
+  void set_health(unsigned int health);
+  void draw(sf::RenderWindow& window);
+
+ private:
+  void setup_text();
+  void update_string();
+
+  unsigned int mHealth;
+  unsigned int mTotalHealth;
+  sf::Font mFont;
+  sf::Text mText;
+};
+
