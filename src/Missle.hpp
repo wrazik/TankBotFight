b0770d12ca231d219eb19fa9a2f11dbcef791d74
Dawid Biliński <dawid.bilinski.99@gmail.com>
2023-09-03 11:47:53 +0200

    add damage field in Missle class with getter

--- src/Missle.hpp
+++ src/Missle.hpp
@@ -4,16 +4,18 @@
 #include "MovementState.hpp"
 class Missle {
  public:
-  Missle(sf::Texture& texture, const MovementState& state);
+  Missle(sf::Texture& texture, const MovementState& state, unsigned int damage);
   void draw(sf::RenderWindow& window);
   void update();
   [[nodiscard]] sf::Vector2f get_pos() const;
+  [[nodiscard]] unsigned int get_damage() const;
 
   bool operator==(const Missle& rhs) const;
 
  private:
   sf::Sprite mSprite;
   float mSpeed = 20.0f;
+  unsigned int mDamage;
 
   sf::Vector2f mPos;
   float mAngle;
