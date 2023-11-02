b427c5ba2dcfb25003503199ce93fa331e05a678
Bartosz Rozenkop <bartez33a@gmail.com>
2023-11-02 18:38:20 +0100

    Added MenuItem base class for Button class
    - added MenuItem base class for Button class
    - added MenuItem.cpp to CMakeLists

---
+++ src/MenuItem.cpp
@@ -0,0 +1,3 @@
+#include "MenuItem.hpp"
+
+MenuItem::MenuItem(sf::Vector2f position) : mPosition{position} {}
---
+++ src/MenuItem.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+class MenuItem {
+  sf::Vector2f mPosition;
+
+ public:
+  MenuItem(sf::Vector2f position);
+};
